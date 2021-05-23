d49e3730746d120ebb44c3dd1c132f962b3797ce
Christian Wolter Grann <202009898@post.au.dk>
2021-05-23 15:53:18 +0200

    datablock (testing)

--- datablocks.py
+++ datablocks.py
@@ -27,11 +27,20 @@ def store_table(temp_table):
         cur = conn.cursor()
         cur.execute("""
         CREATE TABLE IF NOT EXISTS t1{};""".format(tuple(temp_table)))
-        print("""
-        CREATE TABLE IF NOT EXISTS t1{};""".format(tuple(temp_table)))
         conn.commit()
-        
 
+def reset_database():
+    """Deletes all stored tables in database"""
+    with sqlite3.connect(
+        __MEASUREMENTS_DB, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES
+    ) as conn:
+        cur = conn.cursor()
+        cur.execute("""
+        SELECT * FROM sqlite_master WHERE type = 'table';
+        """)
+        h = cur.fetchall()
+        print(h)
 
 if __name__ != "__main__":
     create_database()
+    reset_database()
