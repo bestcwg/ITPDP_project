d705065ad79167a762bd28a038fb25468d7aca89
Henning <henninglin@live.dk>
2021-05-24 11:09:06 +0200

    algorithm changes v1.1

--- algorithm.py
+++ algorithm.py
@@ -1,18 +1,16 @@
 import functional_dependencies as FD
 from functional_dependencies.functional_dependencies import FDSet
 
-#data = {"A","B","C","D","E","F","G","H","I","J"}
+data = {"A","B","C","D","E"}
 
-fd1 = FD.FD("A","B")
-fd2 = FD.FD("A","D")
-fd3 = FD.FD("B","F")
-fd4 = FD.FD("F","G")
-fd5 = FD.FD("D","I")
-
-data = {fd1, fd2, fd3, fd4, fd5}
+fd1 = FD.FD({"A"},{"B","C","D"})
+#fd2 = FD.FD("A","E")
 
 fd1set = fd1.rminimize()
 
 print(fd1set)
-print(len(fd1set) == 1)
-print(fd1.attributes() == {"A","B"})
+print(len(fd1set) == 3)
+print(fd1.attributes())
+print(FDSet.key(fd1set))
+print(FDSet.closure(fd1set))
+
