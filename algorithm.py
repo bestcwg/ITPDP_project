a333b20f7eec976257201a9d837f4acbd8a9e1eb
Henning <henninglin@live.dk>
2021-05-24 10:45:39 +0200

    print algorithm

--- algorithm.py
+++ algorithm.py
@@ -3,15 +3,16 @@ from functional_dependencies.functional_dependencies import FDSet
 
 #data = {"A","B","C","D","E","F","G","H","I","J"}
 
-fd1 = FD.FD({"A","B"},{"C"})
-fd2 = FD.FD({"A"},{"D","E"})
-fd3 = FD.FD({"B"},{"F"})
-fd4 = FD.FD({"F"},{"G","H"})
-fd5 = FD.FD({"D"},{"I","J"})
+fd1 = FD.FD("A","B")
+fd2 = FD.FD("A","D")
+fd3 = FD.FD("B","F")
+fd4 = FD.FD("F","G")
+fd5 = FD.FD("D","I")
 
 data = {fd1, fd2, fd3, fd4, fd5}
 
-fd1set = data
+fd1set = fd1.rminimize()
 
 print(fd1set)
-
+print(len(fd1set) == 1)
+print(fd1.attributes() == {"A","B"})
